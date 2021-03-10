8cd37e2dd066fb5994d381533e9822163ebc174c
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-03-10 23:27:34 +0530

    api key changed

--- src/components/More.js
+++ src/components/More.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./More.css"
 import SlideShow from './SlideShow'
 import Axios from 'axios'
@@ -10,7 +10,7 @@ const More = () => {
         const fetchNews=()=>{
             const requestOne = Axios.get("https://gnews.io/api/v4/top-headlines?topic=entertainment&lang=en&token=a432a2ee1e29bbb1f1a1e86849d6c15e")
             const requestTwo = Axios.get("https://gnews.io/api/v4/top-headlines?topic=business&lang=en&token=a432a2ee1e29bbb1f1a1e86849d6c15e")
-            Axios.all([requestOne, requestTwo,requestThree,requestFour,requestFive]).then(Axios.spread((...responses) => {
+            Axios.all([requestOne, requestTwo]).then(Axios.spread((...responses) => {
                 const data1=responses[0].data
                 const data2=responses[1].data
                 setEntertainment(data1.articles)
--- src/components/Sports.js
+++ src/components/Sports.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Sports.css"
 import Card from "./Card"
 import Axios from "axios"
